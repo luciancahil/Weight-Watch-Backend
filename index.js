8ffa4120bbb602e57d58cc070b7e59c594bb42a0
Roy He <royhe62@yahoo.ca>
2020-09-06 04:02:34 -0700

    established connection to SQL server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//npm install express mysql cors
+//npm install express mysql cors node-forge'
+//dummy, dummy1
+
+var mysql = require('mysql');
+const data = require("./Login");
+var sqlData;
+
+var con = mysql.createConnection({
+  host: data.host,
+  user: data.user,
+  password: data.password,
+  database: "weightTrack"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM userInfo", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result[0].salt);
+  });
+});
