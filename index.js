1128992af9ffecfb0acfaaea1fcf18f5e2bf2fd0
Roy He <royhe62@yahoo.ca>
2020-09-07 01:13:52 -0700

    improved documentation

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const data = require("./Login.json");
 const forge = require('node-forge');
 
 app.use(cors());
-var SQLResults;
 
 
 var con = mysql.createConnection({
@@ -20,19 +19,13 @@ var con = mysql.createConnection({
   database: "weightTrack"
 });
 
-
-//sets SQL results to be the entire contents of the SQL Table
-con.connect(function(err) {
-
-  if (err) throw err;
-  con.query("SELECT * FROM userInfo", function (err, result, fields) {
-    if (err) throw err;
-    SQLResults = result;
-  });
-});
+app.get('/', (req, res) =>{
+  res.end("got to /login for login info")
+})
 
 app.get('/login', (req, res) => {
   const{username, password} = req.query;
+
   //Checks if a username exists
   isValidUsername(username).then(function(result){
     if(result[0].numUN == 0){
@@ -43,14 +36,18 @@ app.get('/login', (req, res) => {
   //checks if password hashed matches the hash stored
   getSaltandHash(username).then(function(result){
     let hashSalt = result[0].salt;                        //salt stored in table
-    let saltedPassword = password + hashSalt;             //password with salt appended
+    let saltedPassword = password + hashSalt;             //user entered password with salt appended
 
     let saltedHsh = result[0].passhash;                   //password hash stored in table
 
+
+    //Runs the salted entered password through sha256
     var md = forge.md.sha256.create();
     md.update(saltedPassword);
     let inputSaltedHash = md.digest().toHex()           //The hash of the salt appeneded to the password the user just entered
 
+
+    //checks to see if the hash of the entered password matches the stored hash 
     if(saltedHsh == inputSaltedHash){
       res.end("Password Correct. Access Granted")
     }else{
@@ -58,15 +55,11 @@ app.get('/login', (req, res) => {
     }
   })
 
-  con.query("SELECT * FROM userInfo", (err, result) => {
-    if(err){
-      return res.end(err)
-    }else{
-      return res.end(result[0])
-    }
-  })
+  res.end("You should never see this");
 });
 
+
+//checks if the username is one we have in the database
 function isValidUsername(uname){
   return new Promise(function(resolve, reject){
   let sqlQ = "SELECT COUNT(*) AS numUN FROM userInfo WHERE username = '" + uname + "'";
@@ -94,7 +87,6 @@ function getSaltandHash(uname){
       })
     })
 }
-
 
 app.listen(PORT, () => {
   console.log('Server Loaded on port ${PORT}');
